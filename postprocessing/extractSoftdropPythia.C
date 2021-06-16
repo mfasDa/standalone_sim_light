5b5dc6048b2e37b3aabc4782ce3d319c640e3cb5
mfasel <markus.fasel@cern.ch>
2021-06-16 12:06:07 +0200

    Add merging stuff for PYTHIA

--- postprocessing/extractSoftdropPythia.C
+++ postprocessing/extractSoftdropPythia.C
@@ -19,7 +19,7 @@ bool hasKey(TList *keylist, const char *keyname) {
     return found;
 }
 
-void extractNormalizedSoftdrop(const char *filename = "jetspectrum_merged.root"){
+void extractSoftdropPythia(const char *filename = "AnalysisResults.root"){
     const double kVerySmall = 1e-5;
     std::vector<double> ptbinning = {15, 20, 30, 40, 50, 60, 80, 100, 120, 140, 160, 180, 200, 240, 500};
     std::unique_ptr<TFile> reader(TFile::Open(filename, "READ")),
@@ -31,7 +31,10 @@ void extractNormalizedSoftdrop(const char *filename = "jetspectrum_merged.root")
         writer->mkdir(obs.data());
         for(auto R : ROOT::TSeqI(2, 7)) {
             reader->cd(obs.data());
-            auto h2D = static_cast<TH2 *>(gDirectory->Get(Form("h%sWeightR%02d", obs.data(), R)));
+            std::string histname;
+            if(obs == "Zg") histname = Form("h%sR%02dAll", obs.data(), R);
+            else histname = Form("h%sAbsR%02dAll", obs.data(), R);
+            auto h2D = static_cast<TH2 *>(gDirectory->Get(histname.data()));
             h2D->SetDirectory(nullptr);
             writer->cd(obs.data());
             gDirectory->mkdir(Form("R%02d", R));
